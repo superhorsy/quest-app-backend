998b08aacf99cbcdf11af1670666f81f82d2d638
kirill.adamuk <kirill.adamuk@acronis.com>
2022-11-26 10:04:12 +0200

    Remove mail send

--- internal/core/helpers/helpers.go
+++ internal/core/helpers/helpers.go
@@ -2,7 +2,6 @@ package helpers
 
 import (
 	"github.com/dgrijalva/jwt-go"
-	"github.com/mailjet/mailjet-apiv3-go"
 	"github.com/superhorsy/quest-app-backend/internal/core/errors"
 	"golang.org/x/crypto/bcrypt"
 	"math/rand"
@@ -70,41 +69,6 @@ func ParseToken(jwtToken string) (jwt.MapClaims, error) {
 	return claims, nil
 }
 
-var htmlBody = `
-<html>
-<head>
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <title>Hello, World</title>
-</head>
-<body>
-   <p>This is an email using Go</p>
-</body>
-`
-
-func SendEmail(email string, name string) error {
-	mailjetClient := mailjet.NewMailjetClient("514b16f4b57472627979bd8b47abeaa4", "946dba2f181cf2a125c601d76dd0352b")
-	messagesInfo := []mailjet.InfoMessagesV31{
-		{
-			From: &mailjet.RecipientV31{
-				Email: "adamuk.kirill@gmail.com",
-				Name:  "Questly",
-			},
-			To: &mailjet.RecipientsV31{
-				mailjet.RecipientV31{
-					Email: email,
-					Name:  name,
-				},
-			},
-			Subject:  "Greetings from Questly.",
-			TextPart: "My first Questly email",
-			HTMLPart: "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Questly</a>!</h3><br />May the delivery force be with you!",
-			CustomID: "AppGettingStartedTest",
-		},
-	}
-	messages := mailjet.MessagesV31{Info: messagesInfo}
-	_, err := mailjetClient.SendMailV31(&messages)
-	if err != nil {
-		return err
-	}
+func SendEmail(_ string, _ string) error {
 	return nil
 }
